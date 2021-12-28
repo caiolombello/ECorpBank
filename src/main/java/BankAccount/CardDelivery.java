45d1e35747811deccd42f356aaaacee376f48f6e
caiobarbieri <caiolvbarbieri@pm.me>
2021-12-28 12:51:09 -0300

    Address & PersonType

--- src/main/java/BankAccount/CardDelivery.java
+++ src/main/java/BankAccount/CardDelivery.java
@@ -1,18 +1,16 @@
 package BankAccount;
 
-import java.util.ArrayList;
-
 public class CardDelivery {
     public static void main(String[] args) {
         Address address = new Address();
 
         Client client = new Client();
 
-        if (client.addresses == null) {
-            client.addresses = new ArrayList<Address>();
+        try {
+            client.addAdress(address);
+            System.out.println("Address added successfully.");
+        } catch (Exception error) {
+            System.err.println("There was an error adding address: " + error.getMessage());
         }
-
-        client.addresses.add(address);
-        System.out.println("Address added successfully.");
     }
 }
--- src/main/java/BankAccount/Client.java
+++ src/main/java/BankAccount/Client.java
@@ -1,16 +1,39 @@
 package BankAccount;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
+
+    public enum PersonType {PHYSICAL, LEGAL}
 
     private Integer code;
     private String name;
     private String cpf;
     private String cnpj;
     private String id;
+    public PersonType type;
 
     public List<Address> addresses;
+
+    public void addAdress(Address address) {
+        if (address == null) {
+            throw new NullPointerException("Address cannot be null.");
+        }
+
+        if (address.getZipCode() == null) {
+            throw new NullPointerException("ZIP Code cannot be null.");
+        }
+
+        getAddresses().add(address);
+    }
+    
+    private List<Address> getAddresses() {
+        if (addresses == null) {
+            addresses = new ArrayList<Address>();
+        }
+        return addresses;
+    }
 
     public String getName() {
         return name;
